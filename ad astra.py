a8c562442f8732251c6397b140e451720e223ffc
JackGlitch101 <135707958+JackGlitch101@users.noreply.github.com>
2023-06-06 22:05:08 +1000

    commit

---
+++ ad astra.py
@@ -0,0 +1,15 @@
+t = 0
+a = 0
+f = 0
+
+t = int(input("what is your Weight: "))
+a = int(input("What is your acceleration: "))
+f = int(input("What is your fuel: "))
+
+
+
+au = round(a/t*(f/10),2)
+print("You can travel " + str(au))
+
+
+
